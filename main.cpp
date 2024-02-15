7aef648d3824aa344be7b6f60ad1e5bb214ed196
Korolnastasia <korolnastasia@gmail.com>
2024-02-15 13:44:47 +0500

    Add all

---
+++ main.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <cmath>
+#include <vector>
+#define _USE_MATH_DEFINES
+#include <math.h>
+
+#ifdef USE_FLOAT
+using DataType = float;
+#else
+using DataType = double;
+#endif
+
+int main() {
+    const int arraySize = 10000000; 
+    std::vector<DataType> sineArray(arraySize);
+    double circle = (2 * M_PI / arraySize);
+    for (int i = 0; i < arraySize; ++i) {
+        double angle = circle * i;
+        sineArray[i] = sin(angle);
+    }
+
+    DataType sum = 0.0;
+    for (DataType value : sineArray) {
+        sum += value; 
+    }
+
+    std::cout << "Sum of sin values: " << sum << std::endl;
+
+    return 0;
+}
